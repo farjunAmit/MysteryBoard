307429e86b1a5e066f4698d5edb5f4cd5d0dadd8
FarjunAmit <farjunamit@gmail.com>
2025-12-02 12:42:11 +0200

    Initial clean commit

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+﻿const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.use(cors());
+let scenarios = [
+	{
+		id: '1',
+		name: 'מי רצח את קונוליוס פאדג',
+		description: 'תרחיש לדוגמא',
+		minPlayers: 6,
+		maxPlayers: 10,
+		imageUrl: 'https://placehold.co/400x250?text=Example',
+	},
+];
+
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'pong from backend 🧩' });
+});
+
+app.get('/api/scenarios', (req, res) => {
+  res.json(scenarios);
+});
+
+app.post('/api/scenarios', (req, res) => {
+	const { name, description, minPlayers, maxPlayers, imageUrl } = req.body;
+	const newScenario = {
+		id: Date.now().toString(),
+		name: name,
+		description: description,
+		minPlayers: minPlayers,
+		maxPlayers: maxPlayers,
+		imageUrl: imageUrl,
+
+	};
+	
+	scenarios.push(newScenario);
+	res.status(201).json(newScenario);
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend server is running on http://localhost:${PORT}`);
+});
