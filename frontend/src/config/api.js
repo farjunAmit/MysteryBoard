0953db0eb73bd95b3174417c0a0401b013d3e3e3
FarjunAmit <farjunamit@gmail.com>
2026-01-06 14:37:28 +0200

    debug: log API_BASE_URL on prod

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,8 @@
 // src/api/config.js
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
-
+  
+console.log(process.env.REACT_APP_API_BASE_URL);
 export const API_PATHS = {
   auth: "/api/auth",                    // auth (register, login)
   scenarios: "/api/scenarios",          // admin
