41d24bacea79cefea9a78698be58c53933387989
FarjunAmit <farjunamit@gmail.com>
2026-01-06 19:16:40 +0200

    remove logs befor production

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,7 +2,6 @@
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
   
-console.log(process.env.REACT_APP_API_BASE_URL);
 export const API_PATHS = {
   auth: "/api/auth",                    // auth (register, login)
   scenarios: "/api/scenarios",          // admin
